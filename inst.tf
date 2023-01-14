26965fd124ac13482fbbb71cf801c8565e2b3257
sakir2141 <117640302+sakir2141@users.noreply.github.com>
2023-01-14 10:54:12 +0530

    Update inst.tf

--- inst.tf
+++ inst.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "DevOps" {
-  ami           = "ami-0cca134ec43cf708f"
+  ami           = "ami-06c3426233c180fef"
   instance_type = "t2.micro"
 
   tags = {
